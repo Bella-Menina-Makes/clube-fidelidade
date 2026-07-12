fe881c2b5eddf774ab801ad44b849ae7f9f58a18
bellameninamakesgo <bellameninamakes@gmail.com>
2026-07-11 21:10:48 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,6 +2,4 @@
 
 const supabaseUrl = 'https://vtmyjkganxuehdzcbgj.supabase.co'; 
 const supabaseKey = 'sb_publishable_6JRlAW3h8YQKFCjPQ3ZzTQ_XNSZyho9';
-
-
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
